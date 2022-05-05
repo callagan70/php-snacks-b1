f4ded6f3cd591be40f9ae58209853dcd204438dd
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-05 19:17:37 +0100

    Snecchino 7 Okey

--- index.php
+++ index.php
@@ -29,6 +29,48 @@ $matches = [
 
 ];
 
+$allievi = [
+    [
+        'nome' => 'Stefano',
+        'cognome' => 'Achino',
+        [
+        'latino' => 70,
+        'matematica' => 45,
+        'italiano' => 34
+        ],
+    ],
+
+    [
+        'nome' => 'Fabio',
+        'cognome' => 'Dolomiti ',
+        [
+        'latino' => 74,
+        'matematica' => 55,
+        'italiano' => 44
+        ],
+    ],
+
+    [
+        'nome' => 'Marco',
+        'cognome' => 'Virtus',
+        [
+        'latino' => 90,
+        'matematica' => 15,
+        'italiano' => 14
+        ],
+    ],
+
+    [
+        'nome' => 'Alessandro',
+        'cognome' => 'Germani',
+        [
+        'latino' => 23,
+        'matematica' => 35,
+        'italiano' => 35
+        ],
+    ],
+
+];
 ?>
 
 
@@ -74,6 +116,25 @@ echo $matches[$i]['team1'] . " - " . $matches[$i]['team2'] . " | " . $matches[$i
     var_dump($arrayNum)
 ?>
 
+<h1> Snecchino 7</h1> 
+  <br>
+  <h3>Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.</h3>
+  <br>
+
+  <?php
+    for($i=0; $i < count($allievi); $i++){
+        echo "Nome: " . $allievi[$i]['nome'] ."<br>";
+        echo "Cognome: " . $allievi[$i]['cognome'] ."<br>";
+        echo "-- Voto di latino: " . $allievi[$i][0]['latino'] ."<br>";
+        echo "-- Voto di matematica: " . $allievi[$i][0]['matematica'] ."<br>";
+        echo "-- Voto di italiano: " . $allievi[$i][0]['italiano'] ."<br>";
+        $media = floor(($allievi[$i][0]['latino'] + $allievi[$i][0]['matematica'] + $allievi[$i][0]['italiano']) / count ($allievi[$i][0]));
+        echo "------ Media: ". $media ."<br>"."<br>";
+    };
+
+
+
+?>
 
 </body>
 </html>
